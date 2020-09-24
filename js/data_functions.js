817e079443069924900e54bb02a8d9618df5e3ec
wallWrecker <sinantakboy@gmail.com>
2020-09-24 11:09:55 +0800

    refactored code

--- js/data_functions.js
+++ js/data_functions.js
@@ -6,12 +6,16 @@ import { transactions } from "./data.js";
 // Funcition that displays 'active only' transaction.
 let data = transactions;
 
-console.log(data);
+function return_unpaid() {
+  let result = data.filter((ele) => ele.status == "unpaid");
+  return result;
+}
 
-function find_unpaid() {
+function find_paid() {
   let result = data.filter((ele) => ele.status == "paid");
   return result;
 }
+
 function find_id(id) {
   let data = transactions.reverse();
   return data.find((ele) => ele.id == id);
@@ -19,15 +23,13 @@ function find_id(id) {
 
 function update_transaction(id, status) {
   data.forEach((ele) => {
-    if( ele['id'] == 1) {
-      console.log("Got it! " + ele['id'] +
-        " " + ele['name']);
+    if (ele["id"] == 1) {
+      console.log("Got it! " + ele["id"] + " " + ele["name"]);
     } else {
-      return
+      return;
     }
-
   });
 }
 
 // Export
-export {find_unpaid, find_id, update_transaction };
+export { return_unpaid, find_id, update_transaction };
