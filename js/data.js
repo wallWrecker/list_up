fcdb9c9d22f5ea11bdbb84bfad2629380d702024
wallWrecker <sinantakboy@gmail.com>
2020-09-24 11:45:28 +0800

    no modification have done

--- js/data.js
+++ js/data.js
@@ -1,29 +1,28 @@
-let transactions = 
-[
+let transactions = [
   {
-    "id": 1,
-    "name": "Christian Paul",
-    "promo": "TM50X",
-    "amount": 50,
-    "date": "13/08/2020",
-    "status": "paid"
+    id: 1,
+    name: "Christian Paul",
+    promo: "TM50X",
+    amount: 50,
+    date: "13/08/2020",
+    status: "paid",
   },
   {
     id: 2,
-    name:"Girlie",
-    promo:"TM50X",
-    amount:50,
-    date:"14/08/2020",
-    status:"unpaid",
+    name: "Girlie",
+    promo: "TM50X",
+    amount: 50,
+    date: "14/08/2020",
+    status: "unpaid",
   },
   {
     id: 3,
-    name:"Maria",
-    promo:"G100X",
-    amount:100,
-    date:"15/08/2020",
-    status:"paid",
-  }
-]
+    name: "Maria",
+    promo: "G100X",
+    amount: 100,
+    date: "15/08/2020",
+    status: "paid",
+  },
+];
 
 export { transactions };
