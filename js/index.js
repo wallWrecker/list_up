371dac0f048834a1e0453cad6cb1cb135d7739c8
wallWrecker <sinantakboy@gmail.com>
2020-09-24 11:16:04 +0800

    adding and removing import functionalities and arranges sequence of code for good loading times

--- js/index.js
+++ js/index.js
@@ -2,22 +2,24 @@
 import { transactions } from "./data.js";
 // imported Functionalities
 import {
-  find_unpaid,
+  return_unpaid,
   find_id,
   update_transaction,
 } from "./data_functions.js";
 
-import {display_all} from './user_interaction.js';
+import {
+  inputs_container,
+  display_all,
+  activate_button_filter,
+  mini_button_filters,
+  clear_up_inputs,
+} from "./user_interaction.js";
 
-display_all(transactions.reverse());
+localStorage.setItem("transaction", JSON.stringify(transactions));
+let x = JSON.parse(localStorage.getItem("transaction"));
+clear_up_inputs(inputs_container);
+// Initially assign all records filter buttons by default.
+mini_button_filters[0].classList.add("is-link");
+display_all(transactions);
 
-let button = document.getElementById("button");
-
-button.addEventListener("click", function () {
-  let data = find_unpaid().reverse();
-  data.forEach((element) => {
-    for (let prop in element) {
-      console.log(`property: ${prop} , value: ${element[prop]}`);
-    }
-  });
-});
+console.table(x);
